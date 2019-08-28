f50aeaa0e54852a16d81f9a0d3900802e9c1b0cb
罗平 <pingluo@kugou.net>
2019-08-28 09:59:53 +0800

    Initial Commit

---
+++ LeetCode/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LeetCode
+//
+//  Created by 罗平 on 2019/8/28.
+//  Copyright © 2019 罗平. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
